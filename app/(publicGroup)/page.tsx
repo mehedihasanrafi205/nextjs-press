902ea1dabac2a3debda63521ffd2a3148064e62e
Mehedi Hasan Rafi <mehedihasanrafi205@gmail.com>
2026-07-17 17:46:51 +0600

    feat: integrate shadcn/ui and setup initial UI components

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,11 +1,14 @@
-
-
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   console.log("Root Route");
   return (
     <div>
       Hello NextJS
+      <Button
+      size={'xs'}
+      variant={"destructive"}
+      >Click me</Button>
     </div>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,13 @@
 import type { Metadata } from "next";
 
 import "./globals.css";
+import { IBM_Plex_Sans, Source_Sans_3 } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const sourceSans3Heading = Source_Sans_3({subsets:['latin'],variable:'--font-heading'});
+
+const ibmPlexSans = IBM_Plex_Sans({subsets:['latin'],variable:'--font-sans'});
+
 
 // export const metadata: Metadata = {
 //   title: "Create Next App",
@@ -13,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={` h-full antialiased`}>
+    <html lang="en" className={cn("h-full", "antialiased", "font-sans", ibmPlexSans.variable, sourceSans3Heading.variable)}>
       <body className="min-h-full flex flex-col">
         {/* Navbar */}
         {children}
